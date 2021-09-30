25242675a17b6e745b640de63f432041d12df204
Vinh <ducvinh.truong2001@gmail.com>
2021-09-30 10:30:19 +0700

    Fix filter select

--- src/components/searchAndFilter/filter/FilterBar.js
+++ src/components/searchAndFilter/filter/FilterBar.js
@@ -4,7 +4,7 @@ import FilterSelectList from './select/FilterSelectList';
 
 function FilterBar() {
     return (
-        <div className="sticky top-16 h-12 flex items-center justify-center bg-white border-b border-gray-300 shadow-md overflow-x-auto">
+        <div className="sticky top-16 h-12 flex items-center justify-center bg-white border-b border-gray-300 shadow-md">
             <FilterSelectList />
             <FilterLinkList />
         </div>
