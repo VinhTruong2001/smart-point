6230f9a13e1284ceea0a00771d45f04a03337392
VinhTruong2001 <ducvinh.truong2001@gmail.com>
2021-11-15 11:42:56 +0700

    Delete computer img

--- src/components/searchAndFilter/search/SearchBar.js
+++ src/components/searchAndFilter/search/SearchBar.js
@@ -3,7 +3,7 @@ import SearchIcon from '@mui/icons-material/Search';
 
 function SearchBar() {
     return (
-        <form className="xl:min-w-[650px] flex py-3 px-2 bg-white rounded-lg">
+        <form className="min-w-[300px] xl:min-w-[650px] flex py-3 px-2 bg-white rounded-lg">
             <input className="flex-1 px-2 outline-none" type="search" name="search" placeholder="Tìm kiếm template"/>
             <div className="cursor-pointer">
                 <SearchIcon />
